c1288ef26e93326c57452d8b7e0badd2a0eb318b
Manikonda123 <r_sahit@yahoo.com>
2022-09-12 19:32:12 -0400

    Initial commit

---
+++ app/src/main/java/com/example/group6_incalss04/registration_activity.java
@@ -0,0 +1,14 @@
+package com.example.group6_incalss04;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class registration_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+    }
+}
